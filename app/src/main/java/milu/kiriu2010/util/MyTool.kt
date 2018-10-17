0067c07b98205be0ce249da4d61251852d980fd7
milu(Mitsuharu Kiriu) <milu.kiriu2010@gmail.com>
2018-10-18 08:35:22 +0900

    update

--- app/src/main/java/milu/kiriu2010/util/MyTool.kt
+++ app/src/main/java/milu/kiriu2010/util/MyTool.kt
@@ -8,10 +8,13 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-// 2018.09.15  String(ISO8601+RFC3339)をDateへ変換
 // 2018.10.06  rawファイルの中身をString型に変換
+// 2018.09.25  今日の日付をYYYYMMDD形式で取得
+// 2018.09.15  String(ISO8601+RFC3339)をDateへ変換
 class MyTool {
     companion object {
+        // ----------------------------------------
+        // 2018.10.06
         // ----------------------------------------
         // rawファイルの中身をString型に変換
         // ----------------------------------------
@@ -29,6 +32,36 @@ class MyTool {
 
             return sb.toString()
         }
+        
+        // ----------------------------------------
+        // 2018.09.25
+        // ----------------------------------------
+        // 今日の日付をYYYYMMDD形式で取得
+        // ----------------------------------------
+        fun getToday( zone: String = "Asia/Tokyo" ): String {
+            // Date()はUTCなので、タイムゾーンを"Asia/Tokyo"にして変換する
+            val date = Date()
+            val dateFormat = SimpleDateFormat("yyyyMMdd")
+            dateFormat.timeZone = TimeZone.getTimeZone(zone)
+            return dateFormat.format(date)
+        }
+
+        // ----------------------------------------
+        // 2018.09.15
+        // ----------------------------------------
+        // RFC3339
+        //   2018-08-28T19:00:00+09:00
+        //   2018-09-14T21:46:00Z
+        // ----------------------------------------
+        fun rfc3339date(str: String): Date {
+            try {
+                val formatterRFC3339_1 = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz", Locale.US)
+                return formatterRFC3339_1.parse(str)
+            } catch ( parseEx: ParseException) {
+                val formatterRFC3339_2 = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
+                return formatterRFC3339_2.parse(str)
+            }
+        }
 
         // ----------------------------------------
         // Exceptionを文字列に変換
@@ -40,23 +73,6 @@ class MyTool {
             pw.flush()
             return sw.toString()
         }
-
-        // ----------------------------------------
-        // String(ISO8601+RFC3339)をDateへ変換
-        // ----------------------------------------
-        // RFC3339
-        //   2018-08-28T19:00:00+09:00
-        //   2018-09-14T21:46:00Z
-        // ----------------------------------------
-        fun rfc3339date(str: String): Date {
-            try {
-                val formatterRFC3339_1 = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz", Locale.US)
-                return formatterRFC3339_1.parse(str)
-            } catch ( parseEx: ParseException ) {
-                val formatterRFC3339_2 = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.US)
-                return formatterRFC3339_2.parse(str)
-            }
-        }
-
+        
     }
 }
