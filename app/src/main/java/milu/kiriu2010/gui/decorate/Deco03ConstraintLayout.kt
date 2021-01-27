3805647d997f850cbce6551ab82762e4d49dda81
milukiriu2010 <milu.kiriu2010@gmail.com>
2021-01-27 16:54:50 +0900

    version up

--- app/src/main/java/milu/kiriu2010/gui/decorate/Deco03ConstraintLayout.kt
+++ app/src/main/java/milu/kiriu2010/gui/decorate/Deco03ConstraintLayout.kt
@@ -11,7 +11,7 @@ import android.util.Log
 class Deco03ConstraintLayout
     @JvmOverloads
     constructor(
-            context: Context?,
+            context: Context,
             attrs: AttributeSet? = null,
             defStyleAttr: Int = 0
     )
